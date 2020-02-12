e56d886808e5b9d717c18c5d7f5106bd74ae5db6
RJ <RJ>
2020-02-12 10:47:43 -0500

    Started making the CustomInspector

---
+++ CustomInspectorWindow/Assets/CustomInspectorWindowBlock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomInspectorWindowBlock : MonoBehaviour
+{
+    public bool theBool = false;
+    public int theInt = 0;
+    public float theFloat = 0.0f;
+    public Vector2 theVec2 = new Vector2(0, 0);
+    public Vector3 theVec3 = new Vector3(0, 0, 0);
+    public char theChar = ' ';
+    public string theString = " ";
+    public GameObject theObj = null;
+    public Transform theTransform = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ CustomInspectorWindow/Assets/Editor/CustomInspectorMaker.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public enum VariableList
+{
+    Bool = 0,
+    Int,
+    Float,
+    Vector2,
+    Vector3,
+    Char,
+    String,
+    GameObject,
+    Transform,
+}
+
+public class CustomInspectorMaker : EditorWindow
+{
+    List<VariableList> theList = new List<VariableList>();
+    List<CustomInspectorWindowBlock> varList = new List<CustomInspectorWindowBlock>();
+
+    [MenuItem("Tools/Custom Inspector Maker")]
+    public static void ShowWindow()
+    {
+        CustomInspectorMaker window = (CustomInspectorMaker)EditorWindow.GetWindow(typeof(CustomInspectorMaker));
+        window.Show();
+    }
+
+    void OnGUI()
+    {
+        GUILayout.Label("Base Settings", EditorStyles.boldLabel);
+
+        for (int i = 0; i < varList.Count; ++i)
+        {
+            theList[i] = (VariableList)EditorGUILayout.EnumPopup("Variable", theList[i]);
+            if (theList[i] == VariableList.Bool)
+                varList[i].theBool = EditorGUILayout.Toggle("Toggle", varList[i].theBool);
+            if (theList[i] == VariableList.Int)
+                varList[i].theInt = EditorGUILayout.IntField("Toggle", varList[i].theInt);
+        }
+
+        if (GUILayout.Button("This ButtoN"))
+        {
+            CustomInspectorWindowBlock block = new CustomInspectorWindowBlock();
+            varList.Add(block);
+
+            theList.Add(new VariableList());
+        }
+    }
+}

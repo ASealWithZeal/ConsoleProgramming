bb14493bb1aa45e7ee7929cb1d59cfbf1c3b3349
RJ <RJ>
2020-02-19 10:44:38 -0500

    Started working on the layout more. May have to change later.

--- CustomInspectorWindow/Assets/Editor/CustomInspectorMaker.cs
+++ CustomInspectorWindow/Assets/Editor/CustomInspectorMaker.cs
@@ -26,6 +26,10 @@ public class CustomInspectorMaker : EditorWindow
 
         for (int i = 0; i < varList.Count; ++i)
         {
+            //The rectangle is drawn in the Editor (when MyScript is attached) with the width depending on the value of the Slider
+            EditorGUI.DrawRect(new Rect(2.5f, (2.5f + (85 * i)), Screen.width - 3.5f, 82.5f), Color.white);
+
+            GUILayout.Space(5.0f);
             //GUILayout.BeginArea(new Rect(100, 10 + (100 * i), position.width, 200));
             DisplayGeneralData(i);
 
@@ -41,8 +45,26 @@ public class CustomInspectorMaker : EditorWindow
                 CreateFloatData(i);
                 CheckMinMax(i, MinMaxType.Float);
             }
+            else if (varList[i].type == VariableList.Vector2)
+            {
+                CreateVec2Data(i);
+            }
+            else if (varList[i].type == VariableList.Vector3)
+            {
+                CreateVec3Data(i);
+            }
 
-            GUILayout.Space(25.0f);
+            else
+            {
+                GUILayout.Space(40.0f);
+            }
+
+            if (GUILayout.Button("Destroy This Variable"))
+            {
+                varList.Remove(varList[i]);
+            }
+
+            GUILayout.Space(2.5f);
             //GUILayout.EndArea();
         }
 
@@ -65,6 +87,8 @@ public class CustomInspectorMaker : EditorWindow
         GUILayout.Space(10.0f);
         varList[i].type = (VariableList)EditorGUILayout.EnumPopup(varList[i].type);
 
+        GUILayout.Space(25.0f);
+
         // Protection level
         GUILayout.Label("Protection");
         GUILayout.Space(10.0f);
@@ -76,28 +100,60 @@ public class CustomInspectorMaker : EditorWindow
     {
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("Default");
-        GUILayout.Space(10.0f);
+        GUILayout.Space(15.0f);
         varList[i].theBool = EditorGUILayout.Toggle(varList[i].theBool);
         EditorGUILayout.EndHorizontal();
     }
 
     private void CreateIntData(int i)
     {
+        // Default value
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("Default");
+        GUILayout.Space(15.0f);
         varList[i].theInt = EditorGUILayout.IntField(varList[i].theInt);
+
+        GUILayout.Space(25.0f);
+
+        // Min/Max toggle
         GUILayout.Label("Min/Max");
+        GUILayout.Space(15.0f);
         varList[i].minMax = EditorGUILayout.Toggle(varList[i].minMax);
         EditorGUILayout.EndHorizontal();
     }
 
     private void CreateFloatData(int i)
     {
+        // Default value
         EditorGUILayout.BeginHorizontal();
         GUILayout.Label("Default");
+        GUILayout.Space(15.0f);
         varList[i].theFloat = EditorGUILayout.FloatField(varList[i].theFloat);
+
+        GUILayout.Space(25.0f);
+
+        // Min/Max toggle
         GUILayout.Label("Min/Max");
+        GUILayout.Space(15.0f);
         varList[i].minMax = EditorGUILayout.Toggle(varList[i].minMax);
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void CreateVec2Data(int i)
+    {
+        // Default value
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Space(5.0f);
+        varList[i].theVec2 = EditorGUILayout.Vector2Field("Default", varList[i].theVec2);
+        EditorGUILayout.EndHorizontal();
+    }
+
+    private void CreateVec3Data(int i)
+    {
+        // Default value
+        EditorGUILayout.BeginHorizontal();
+        GUILayout.Space(5.0f);
+        varList[i].theVec3 = EditorGUILayout.Vector3Field("Default", varList[i].theVec3);
         EditorGUILayout.EndHorizontal();
     }
 
@@ -111,7 +167,11 @@ public class CustomInspectorMaker : EditorWindow
             if (type == MinMaxType.Int)
             {
                 GUILayout.Label("Minimum");
+                GUILayout.Space(5.0f);
                 varList[i].min = EditorGUILayout.IntField((int)varList[i].min);
+
+                GUILayout.Space(25.0f);
+
                 GUILayout.Label("Maximum");
                 varList[i].max = EditorGUILayout.IntField((int)varList[i].max);
             }
@@ -120,12 +180,19 @@ public class CustomInspectorMaker : EditorWindow
             else if (type == MinMaxType.Float)
             {
                 GUILayout.Label("Minimum");
+                GUILayout.Space(5.0f);
                 varList[i].min = EditorGUILayout.FloatField(varList[i].min);
+
+                GUILayout.Space(25.0f);
+
                 GUILayout.Label("Maximum");
                 varList[i].max = EditorGUILayout.FloatField(varList[i].max);
             }
 
             EditorGUILayout.EndHorizontal();
         }
+
+        else
+            GUILayout.Space(20.0f);
     }
 }
